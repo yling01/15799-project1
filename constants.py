748f0e6516a801f4b807b1d04bdf0ef2069c5028
Yingjie Ling <yingjie.ling@tufts.edu>
2022-03-15 10:04:57 -0400

    removed workloadProfile class and commented constants

--- constants.py
+++ constants.py
@@ -1,5 +1,4 @@
-# constant file
-
+# string constant
 STATEMENT = "statement"
 WHERE = "where"
 SELECT = "select"
@@ -8,7 +7,22 @@ DELETE = "delete"
 INSERT = "insert"
 BEGIN = "begin"
 COMMIT = "commit"
+
+# cutoff constant
+
+"""
+If a column is updated more than UPDATE_CUTOFF.
+a candidate index is no longer considered if 
+    * a composite index is referenced less than COMPOSITE_REFERENCE_CUTOFF_HIGH
+    * a simple index is referenced less than SIMPLE_REFERENCE_CUT_OFF_HIGH
+"""
+UPDATE_CUTOFF = 0.05
 COMPOSITE_REFERENCE_CUTOFF_HIGH = 0.2
 SIMPLE_REFERENCE_CUT_OFF_HIGH = 0.2
+
+"""
+If a column (or columns) is used more than REFERENCE_CUTOFF_LOW 
+it will be considered as a candidate index.
+"""
 REFERENCE_CUTOFF_LOW = 0.1
-UPDATE_CUTOFF = 0.05
+
--- workloadProfile.py
+++
@@ -1,5 +0,0 @@
-class profile:
-    def __init__(self, select, update, delete):
-        self.num_select = select
-        self.update = update
-        self.delete = delete
